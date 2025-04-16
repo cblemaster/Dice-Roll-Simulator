f7e1539788c4688dd987f5ed819afd342dddd21f
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-15 21:07:54 -0400

    add result models

---
+++ src/Simulator.Models/RollResult.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Simulator.Models;
+
+internal sealed class RollResult
+{
+    private readonly uint _sides;
+
+    internal uint Result { get; }
+
+    internal RollResult(uint sides, uint result)
+    {
+        _sides = sides;
+        Result = result;
+    }
+}
---
+++ src/Simulator.Models/TallyResult.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Simulator.Models;
+
+public sealed class TallyResult
+{
+    private readonly string _dieResults;
+    private readonly uint _diceTotal;
+    private readonly int _modifier;
+    private readonly int _rollTotal;
+
+    internal TallyResult(string dieResults, uint diceTotal, int modifier, int rollTotal)
+    {
+        _dieResults = dieResults;
+        _diceTotal = diceTotal;
+        _modifier = modifier;
+        _rollTotal = rollTotal;
+    }
+}

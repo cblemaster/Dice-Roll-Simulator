534c838e54135f39f7835927b68fc2e32e14abf8
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-15 21:10:26 -0400

    add dice models

---
+++ src/Simulator.Models/Dice.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Simulator.Models;
+
+public sealed class Dice
+{
+    private readonly IEnumerable<RollResult> _diceResult = [];
+    private readonly int _modifier;
+
+    public Dice(uint sides, uint count, int modifier)
+    {
+        // TODO: validate
+        for (int i = 0; i < count - 1; i++)
+        {
+            _diceResult = _diceResult.Append(new Die(sides).Roll());
+        }
+        _modifier = modifier;
+    }
+
+    public TallyResult TallyDice()
+    {
+        uint diceTotal = (uint)_diceResult.Sum(d => d.Result);
+        string dieResults = string.Join(" ", _diceResult.Select(d => d.Result.ToString()));
+        int rollTotal = (int)diceTotal + _modifier;
+        return new(dieResults, diceTotal, _modifier, rollTotal);
+    }
+}
---
+++ src/Simulator.Models/Die.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Simulator.Models;
+
+internal sealed class Die
+{
+    private readonly uint _sides;
+
+    internal Die(uint sides) => _sides = sides;  // TODO: validate
+
+    internal RollResult Roll()
+    {
+        Random rand = new();
+        int result = rand.Next(1, (int)_sides + 1);
+        return new(_sides, (uint)result);
+    }
+}

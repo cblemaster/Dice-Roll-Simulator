c9860904e6fdc98ab0d19c9bcf54e993c587922b
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-16 21:04:59 -0400

    rename and revise result class

---
+++ src/Simulator.Models/RollResult.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Simulator.Models;
+
+public sealed class RollResult(uint count, uint sides, int modifier, IEnumerable<uint> rolls)
+{
+    private uint Count { get; } = count;
+    private uint Sides { get; } = sides;
+    private IEnumerable<uint> Rolls { get; } = rolls;
+
+    public int Modifier { get; } = modifier;
+    public uint RollsSum => (uint)Rolls.Sum(r => (int)r);
+    public int FinalResult => (int)RollsSum + Modifier;
+    public string ToDefinition()
+    {
+        string s = $"{Count}d{Sides}";
+        if (Modifier > 0)
+        {
+            s = $"{s} + {Math.Abs(Modifier)}";
+        }
+        else if (Modifier < 0)
+        {
+            s = $"{s} - {Math.Abs(Modifier)}";
+        }
+        return s;
+    }
+    public string ToRollsJoined(string separator) => string.Join(separator, Rolls);
+}

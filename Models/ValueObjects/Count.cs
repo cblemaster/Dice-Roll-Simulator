7124d902f3c08b95cd3acfbcd60d1bdb8a2ee53e
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-26 18:09:36 -0400

    add value objects

---
+++ Models/ValueObjects/Count.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Models.ValueObjects;
+
+internal sealed record Count
+{
+    internal uint Value { get; }
+
+    private Count(uint value) => Value = value;
+
+    internal static Count Create(uint value) =>
+        value is 0 or > 10
+            ? throw new ArgumentException("Dice count muct be between one(1) and ten(10).", nameof(value))
+            : new Count(value);
+
+    internal static Count Create(int value) => Create((uint)value);
+}
---
+++ Models/ValueObjects/Modifier.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Models.ValueObjects;
+
+internal sealed record Modifier
+{
+    internal int Value { get; }
+
+    private Modifier(int value) => Value = value;
+
+    internal static Modifier Create(int value) =>
+        value is 0 or > 10
+            ? throw new ArgumentException("Dice count muct be between one(1) and ten(10).", nameof(value))
+            : new Modifier(value);
+}
